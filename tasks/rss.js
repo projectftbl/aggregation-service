1a10cc7737b00f48689cde324319fce29a1b66e8
Johnny Hall <johnny@recipher.co.uk>
2016-08-30 09:28:59 +0100

    Improved image detection

--- tasks/rss.js
+++ tasks/rss.js
@@ -1,7 +1,6 @@
 var rss = require('../lib/apps/aggregations/providers/rss');
 
-var URL = 'http://www.tribalfootball.com/rss/mediafed/general/rss.xml';
-// var URL = 'http://www.independent.co.uk/sport/football/rss';
+var URL = 'http://www.fifa.com/rss/index.xml';
 
 rss({ link: URL }).then(function(posts) {
   console.log(posts);
