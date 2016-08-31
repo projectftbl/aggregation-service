0889789dd4af0ade08865e182af90acfca827a35
Johnny Hall <johnny@recipher.co.uk>
2016-08-31 10:33:31 +0100

    Date fix

--- tasks/rss.js
+++ tasks/rss.js
@@ -1,6 +1,6 @@
 var rss = require('../lib/apps/aggregations/providers/rss');
 
-var URL = 'http://www.fifa.com/rss/index.xml';
+var URL = 'http://www.skysports.com/rss/11095';
 
 rss({ link: URL }).then(function(posts) {
   console.log(posts);
